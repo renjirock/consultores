f515cb62c67f3009a1d5229ebdf5684eb69bbac9
renjirock <mauriciokode@gmail.com>
2020-08-30 18:43:52 -0500

    ejemplo

---
+++ js/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBTt31H_y3oj3HuWtd45zhxFAJNsyYdvEM",
+    authDomain: "vip-correos.firebaseapp.com",
+    databaseURL: "https://vip-correos.firebaseio.com",
+    projectId: "vip-correos",
+    storageBucket: "vip-correos.appspot.com",
+    messagingSenderId: "322901180619",
+    appId: "1:322901180619:web:e34f1120814e119c2edb4e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
